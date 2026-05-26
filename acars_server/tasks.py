0fc516240e3d81cb9d49c2f18edf301444c3c0c4
chssn <33300267+chssn@users.noreply.github.com>
2026-05-26 15:04:00 +0100

    perf: removed some code duplication

--- acars_server/tasks.py
+++ acars_server/tasks.py
@@ -8,6 +8,7 @@ Chris Parkinson (@chssn)
 # Standard Libraries
 import ast
 import re
+from typing import Any, Dict, Union
 
 # Third Party Libraries
 from datetime import datetime as dt, timezone as tz
@@ -21,8 +22,19 @@ vs = inforeq.Vatsim()
 
 def message_parse(msg:sql.StoreAndForward, session:sql.SessionDep):
     """Parse a message"""
-    t_msg = msg
+    send_msg:Dict[str, Any] = {"packet" : None}
+
     # INFOREQ ATIS
+    if msg["msg_type"] == "inforeq":
+        send_msg = {
+            "created": dt.now(tz.utc).timestamp(),
+            "msg_type": "telex",
+            "network": msg["network"],
+            "packet": None,
+            "msg_to": msg["msg_from"],
+            "msg_from": "SYSTEM"
+        }
+
     if (msg["msg_type"] == "inforeq" and
         str(msg["packet"]).startswith("ATIS") and
         re.match(r"[A-Z]{4}", msg["msg_to"])):
@@ -34,73 +46,36 @@ def message_parse(msg:sql.StoreAndForward, session:sql.SessionDep):
             # Deal with a vATIS oddity in returning something that looks like a list but isn't
             if response.startswith("["):
                 fmt = ast.literal_eval(response)
-                pkt = "@".join(fmt)
+                send_msg["packet"] = "@".join(fmt)
             else:
-                pkt = response
-
-            # Format the message to return to the requester
-            t_msg = {
-                "created": dt.now(tz.utc).timestamp(),
-                "msg_type": "inforeq",
-                "network": msg["network"],
-                "packet": pkt,
-                "msg_to": msg["msg_from"],
-                "msg_from": "SERVER"
-            }
+                send_msg["packet"] = response
 
     elif (msg["msg_type"] == "inforeq" and
           str(msg["packet"]).startswith("METAR") and
           re.match(r"[A-Z]{4}", msg["msg_to"])):
 
         if msg["network"] == "vatsim":
-            pkt = inforeq.Vatsim.get_metar(msg["msg_to"])
+            send_msg["packet"] = inforeq.Vatsim.get_metar(msg["msg_to"])
         else:
-            pkt = inforeq.Noaa.metar(msg["msg_to"])
-
-        # Format the message to return to the requester
-        t_msg = {
-            "created": dt.now(tz.utc).timestamp(),
-            "msg_type": "inforeq",
-            "network": msg["network"],
-            "packet": pkt,
-            "msg_to": msg["msg_from"],
-            "msg_from": "SERVER"
-        }
+            send_msg["packet"] = inforeq.Noaa.metar(msg["msg_to"])
 
     elif (msg["msg_type"] == "inforeq" and
           str(msg["packet"]).startswith("TAF") and
           re.match(r"[A-Z]{4}", msg["msg_to"])):
 
-        pkt = inforeq.Noaa.taf(msg["msg_to"])
-
-        # Format the message to return to the requester
-        t_msg = {
-            "created": dt.now(tz.utc).timestamp(),
-            "msg_type": "inforeq",
-            "network": msg["network"],
-            "packet": pkt,
-            "msg_to": msg["msg_from"],
-            "msg_from": "SERVER"
-        }
+        send_msg["packet"] = inforeq.Noaa.taf(msg["msg_to"])
 
     elif (msg["msg_type"] == "inforeq" and
           str(msg["packet"]).startswith("SHORTTAF") and
           re.match(r"[A-Z]{4}", msg["msg_to"])):
 
-        pkt = inforeq.Noaa.shorttaf(msg["msg_to"])
-
-        # Format the message to return to the requester
-        t_msg = {
-            "created": dt.now(tz.utc).timestamp(),
-            "msg_type": "inforeq",
-            "network": msg["network"],
-            "packet": pkt,
-            "msg_to": msg["msg_from"],
-            "msg_from": "SERVER"
-        }
+        send_msg["packet"] = inforeq.Noaa.shorttaf(msg["msg_to"])
 
     # Validate the message content
-    sf_msg = sql.StoreAndForward.model_validate(t_msg)
+    if send_msg["packet"] is None:
+        sf_msg = sql.StoreAndForward.model_validate(msg)
+    else:
+        sf_msg = sql.StoreAndForward.model_validate(send_msg)
 
     # Commit the message to the store
     session.add(sf_msg)
