d4a9541d60c67445a28b049369c514466b2cb88f
chssn <33300267+chssn@users.noreply.github.com>
2026-05-27 14:39:09 +0100

    style: non-conforming lower case vars

--- acars_server/sql.py
+++ acars_server/sql.py
@@ -19,11 +19,11 @@ from sqlmodel import Field, Session, SQLModel, create_engine
 from acars_server import static_data
 
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+SQLITE_FILE_NAME = "database.db"
+SQLITE_URL = f"sqlite:///{SQLITE_FILE_NAME}"
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(SQLITE_URL, connect_args=connect_args)
 
 def create_db_and_tables():
     """Create DB and Tables"""
