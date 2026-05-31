b81ec119e4a09cc8db49c2be41516aa45f5c81d2
chssn <33300267+chssn@users.noreply.github.com>
2026-05-31 20:36:57 +0100

    refactor: split code into logical modules (#25)

---
+++ acars_server/api/routes/users.py
@@ -0,0 +1,84 @@
+"""
+ACARS Server
+User Endpoints
+Chris Parkinson (@chssn)
+"""
+
+#!/usr/bin/env python3
+
+# Standard Libraries
+from datetime import datetime as dt, timezone as tz
+
+# Third Party Libraries
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import RedirectResponse
+
+# Local Libraries
+from acars_server import auth, common, databases, static_data
+from acars_server.api.services.user_services import responses_user_new_network
+
+router = APIRouter()
+# ------------------------------------------------------------------
+# User Endpoints
+# ------------------------------------------------------------------
+@router.get("/user/new/{network}", tags=["User Management"], responses=responses_user_new_network)
+async def auth_new_user(network: str):
+    """Authenticate a new user and generate an API key"""
+    if network in static_data.NETWORKS:
+        if network == "vatsim":
+            v_auth = auth.VatsimAuth()
+            v_url = v_auth.authorise()
+            common.logger.success("Client redirected to VATSIM OAuth")
+            return RedirectResponse(v_url[0])
+
+        error = f"{network} doesn't appear to exist although it really should..."
+        common.logger.error(error)
+        raise HTTPException(
+            status_code=501,
+            detail=error)
+
+    error = (f"{network} is not a recognised network. Needs to be one of "
+             f"{', '.join(static_data.NETWORKS)}")
+    common.logger.error(error)
+    raise HTTPException(
+        status_code=400,
+        detail=error)
+
+@router.get(
+        "/callback/oauth/vatsim/{state}/{code}",
+        response_model=databases.ApiKeyPublic,
+        tags=["Callbacks"])
+async def auth_new_user_callback_vatsim(
+    state:str,
+    code:str,
+    session: databases.SessionDep
+    ):
+    """A callback point for VATSIM"""
+    # Get the access token from VATSIM
+    v_auth = auth.VatsimAuth()
+    v_token = v_auth.get_access_token(code)
+    if v_token[0] != 200:
+        raise HTTPException(status_code=v_token[0], detail=v_token[1]["hint"])
+
+    # Get the user details using the access token
+    v_user = v_auth.get_user_details(v_token[1]["access_token"])
+    if v_user[0] != 200:
+        raise HTTPException(status_code=v_user[0], detail=v_user[1])
+
+    # Generate the API key using the cid
+    v_cid = v_user[1]["data"]["cid"]
+    api_key = auth.Auth().api_key_generator(v_cid, "vatsim")
+
+    # Add the API key to the DB
+    dtnow = dt.now(tz.utc).timestamp()
+    db_data = {
+        "api_key": api_key,
+        "network": "vatsim",
+        "created": dtnow,
+        "last_used": dtnow
+    }
+    db_add = databases.ApiKey.model_validate(db_data)
+    session.add(db_add)
+    session.commit()
+    session.refresh(db_add)
+    return db_add
---
+++ acars_server/api/services/auth_services.py
@@ -0,0 +1,43 @@
+"""
+ACARS Server
+Authentication Services
+Chris Parkinson (@chssn)
+"""
+
+#!/usr/bin/env python3
+
+# Standard Libraries
+from typing import Dict
+
+# Third Party Libraries
+from fastapi import HTTPException
+from sqlmodel import select
+
+# Local Libraries
+from acars_server import auth, common, databases, static_data, networks
+
+async def api_authentication(session:databases.SessionDep, api_key:str) -> Dict[str,str]:
+    """Authenticates an API Key"""
+    db_auth = select(databases.ApiKey).where(databases.ApiKey.api_key == api_key)
+    api_user = session.exec(db_auth).first()
+    if not api_user:
+        common.logger.error("401: API key not recognised")
+        raise HTTPException(status_code=401, detail="Unauthorised")
+    return auth.Auth().api_key_reader(api_key)
+
+async def callsign_verification(user_data) -> str|None:
+    """Validate callsign on various networks"""
+    callsign = None
+    if user_data["network"] == "vatsim":
+        vc = networks.Vatsim()
+        callsign = vc.get_callsign_from_cid(user_data["uid"])
+    elif user_data["network"] == "ivao":
+        pass
+    else:
+        common.logger.error(f"400: Network '{user_data['network']}' is not valid. "
+                    f"Expected one of {', '.join(static_data.NETWORKS)}")
+        raise HTTPException(
+            status_code=400,
+            detail=(f"Network '{user_data['network']}' is not valid. "
+                    f"Expected one of {', '.join(static_data.NETWORKS)}"))
+    return callsign
