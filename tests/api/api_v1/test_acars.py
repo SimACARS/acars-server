9c094868fd8755b4df592c069aebdbd36312f72a
chssn <33300267+chssn@users.noreply.github.com>
2026-06-03 17:43:00 +0100

    tests: acars

--- tests/api/api_v1/test_acars.py
+++ tests/api/api_v1/test_acars.py
@@ -7,7 +7,6 @@ Chris Parkinson (@chssn)
 #!/usr/bin/env python3
 
 # Standard Libraries
-import sys
 from unittest.mock import AsyncMock, patch
 
 # Third Party Libraries
@@ -19,8 +18,8 @@ from tests.api.api_v1.test_dlic import dlic_logon_request
 from tests.factories.messages import MessageFactory
 from tests.factories.user import CallsignFactory, UserApiKeyFactory
 
-class TestAircraftAcars:
-    """Aircraft Logon"""
+class TestAircraftAcarsPoll:
+    """Aircraft Poll"""
     def test_poll_aircraft_no_messages(self, client: TestClient):
         """
         Test that an aircraft can poll the system
@@ -93,3 +92,118 @@ class TestAircraftAcars:
         assert rjson["messages"][0]["msg_type"] == messages.msg_type
         assert rjson["messages"][0]["packet"] == messages.packet
         assert rjson["messages"][0]["network"] == messages.network
+
+    def test_poll_aircraft_with_no_callsign(self, client: TestClient):
+        """
+        Test what happens when no callsign is returned
+        """
+        # Create the database entry
+        aircraft: ApiKey = UserApiKeyFactory()
+        callsign = CallsignFactory()
+
+        # Login
+        response, _ = dlic_logon_request(
+            logon_from=callsign["callsign"],
+            logon_to="EGKK",
+            api_key=aircraft.api_key,
+            endpoint="/dlic/aircraft/logon",
+            client=client
+        )
+
+        # Check the results
+        assert response.status_code == 200
+
+        client.headers.update({"x-key": aircraft.api_key})
+        with patch(
+            "acars_server.api.routes.acars.callsign_verification",
+            new=AsyncMock(return_value=None)
+        ):
+            response_b = client.post("/acars/poll")
+        client.headers.pop("x-key")
+
+        assert response_b.status_code == 403
+
+
+class TestAircraftAcarsTx:
+    """Aircraft Tx"""
+
+    def test_send_message(self, client: TestClient):
+        """Tests sending a message"""
+        # Create the database entry
+        aircraft: ApiKey = UserApiKeyFactory()
+        callsign = CallsignFactory()
+        message: StoreAndForward = MessageFactory(msg_from=callsign["callsign"])
+
+        # Login
+        response, _ = dlic_logon_request(
+            logon_from=callsign["callsign"],
+            logon_to="EGKK",
+            api_key=aircraft.api_key,
+            endpoint="/dlic/aircraft/logon",
+            client=client
+        )
+
+        # Check the results
+        assert response.status_code == 200
+
+        client.headers.update({"x-key": aircraft.api_key})
+        with patch(
+            "acars_server.api.routes.acars.callsign_verification",
+            new=AsyncMock(return_value=callsign["callsign"])
+        ):
+            response_b = client.post("/acars/tx", json=message.model_dump())
+        client.headers.pop("x-key")
+
+        assert response_b.status_code == 201
+
+    def test_send_message_no_callsign(self, client: TestClient):
+        """Tests sending a message"""
+        # Create the database entry
+        aircraft: ApiKey = UserApiKeyFactory()
+        callsign = CallsignFactory()
+        message: StoreAndForward = MessageFactory(msg_from=callsign["callsign"])
+
+        # Login
+        response, _ = dlic_logon_request(
+            logon_from=callsign["callsign"],
+            logon_to="EGKK",
+            api_key=aircraft.api_key,
+            endpoint="/dlic/aircraft/logon",
+            client=client
+        )
+
+        # Check the results
+        assert response.status_code == 200
+
+        client.headers.update({"x-key": aircraft.api_key})
+        with patch(
+            "acars_server.api.routes.acars.callsign_verification",
+            new=AsyncMock(return_value=None)):
+
+            response_b = client.post("/acars/tx", json=message.model_dump())
+        client.headers.pop("x-key")
+
+        assert response_b.status_code == 403
+
+    def test_legacy_send_message(self, client: TestClient):
+        """Tests sending a message"""
+        # Create the database entry
+        aircraft: ApiKey = UserApiKeyFactory()
+        callsign = CallsignFactory()
+        message: StoreAndForward = MessageFactory(msg_from=callsign["callsign"])
+
+        msg = {
+            "logon": aircraft.api_key,
+            "from": message.msg_from,
+            "to": message.msg_to,
+            "type": message.msg_type,
+            "packet": message.packet
+        }
+
+        with patch(
+            "acars_server.api.routes.acars.transmit_a_message",
+            new=AsyncMock(return_value=None)):
+            response = client.get("/acars/connect.html", params=msg)
+        print(response.url)
+
+        assert response.status_code == 200
