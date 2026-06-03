e6e66bb8f3b0159ad0e2076cc0bd6c6e3b99123a
chssn <33300267+chssn@users.noreply.github.com>
2026-06-03 16:09:56 +0100

    perf: removed redundant code

--- acars_server/networks.py
+++ acars_server/networks.py
@@ -24,19 +24,6 @@ class Vatsim:
     def __init__(self) -> None:
         pass
 
-    def corrolate_cid_to_callsign(self, cid:str, callsign:str) -> bool:
-        """
-        Attempts to match the callsign with the CID
-        Returns TRUE if slurper callsign matches provided callsign
-        """
-        response = requests.get(
-            self.SLURPER_URL, params={"cid": cid},
-            timeout=30)
-        r_data = response.text.split(",")
-        if r_data[0] == cid and r_data[1] == callsign:
-            return True
-        return False
-
     def get_callsign_from_cid(self, cid:str) -> Union[str, None]:
         """
         Attempts to match the callsign with the CID
