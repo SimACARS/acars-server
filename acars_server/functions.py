fc14a0c6a614272417172cf59b1d502c01efc738
chssn <33300267+chssn@users.noreply.github.com>
2026-06-04 21:49:04 +0100

    fix: continually adding the same group

--- acars_server/functions.py
+++ acars_server/functions.py
@@ -58,3 +58,45 @@ def load_json_url(url: str, timeout: int = 5, headers: dict|None = None) -> Dict
     except requests.ReadTimeout as err:
         logger.warning(f"{err} - {url}")
     return {}
+
+async def _ensure_group(redis, stream: str, group: str):
+    """Add a group"""
+    try:
+        await redis.xgroup_create(
+            name=stream,
+            groupname=group,
+            id="0",  # only new messages from now
+            mkstream=True
+        )
+        await redis.xtrim(stream, maxlen=1000)
+    except Exception as e:
+        if "BUSYGROUP" not in str(e):
+            raise
+
+_ensure_store = set()
+
+async def ensure_group_once(redis, stream, group):
+    """Ensure the group exists in Redis."""
+    if group in _ensure_store:
+        return
+    _ensure_store.add(group)
+    await _ensure_group(redis, stream, group)
+
+async def stream_messages(redis, stream: str, group: str, consumer: str):
+    """Stream Messages"""
+    while True:
+        resp = await redis.xreadgroup(
+            groupname=group,
+            consumername=consumer,
+            streams={stream: ">"},  # only THIS callsign stream
+            count=10,
+            block=5000,
+        )
+
+        if not resp:
+            continue
+
+        for _, messages in resp:
+            for msg_id, fields in messages:
+                yield msg_id, fields
+                await redis.xack(stream, group, msg_id)
