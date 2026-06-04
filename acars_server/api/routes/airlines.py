36d28a652a6496fcf8621c80c69e9057e9a6efca
chssn <33300267+chssn@users.noreply.github.com>
2026-06-04 13:32:22 +0100

    perf: removed if statement which wasn't needed

--- acars_server/api/routes/airlines.py
+++ acars_server/api/routes/airlines.py
@@ -137,58 +137,56 @@ async def auth_new_airline(
     session: databases.SessionDep
     ):
     """Authenticate a new airline and generate an API key"""
-    if msg.network in static_data.NETWORKS:
+    # Check to see if verified callsign exists for this network already
+    temp_callsign = f"_COY_{msg.airline_callsign}"
+    db_check = select(databases.AirlineApiKey).where(
+        (databases.AirlineApiKey.airline_callsign == temp_callsign),
+        (databases.AirlineApiKey.network == msg.network),)
+    db_result = session.exec(db_check).first()
+    if db_result:
+        return JSONResponse(status_code=403, content={
+            "error": f"{msg.airline_callsign} already exists and is controlled by {msg.domain}"
+            })
 
-        # Check to see if verified callsign exists for this network already
-        temp_callsign = f"_COY_{msg.airline_callsign}"
-        db_check = select(databases.AirlineApiKey).where(
-            (databases.AirlineApiKey.airline_callsign == temp_callsign),
-            (databases.AirlineApiKey.network == msg.network),)
-        db_result = session.exec(db_check).first()
-        if db_result:
-            return JSONResponse(status_code=403, content={
-                "error": f"{msg.airline_callsign} already exists and is controlled by {msg.domain}"
-                })
+    if msg.domain is not None:
+        # Check to see if request is already live
+        all_requests = databases.AirlineVerification.find(
+                    (databases.AirlineVerification.airline_callsign == msg.airline_callsign) &
+                    (databases.AirlineVerification.network == msg.network)
+                ).all()
+        if len(all_requests) > 0:
+            common.logger.info(f"Request already exists for {all_requests[0].model_dump()}")
+            return JSONResponse(
+                status_code=403,
+                content={
+                    "error": "Request already exists",
+                    "data": all_requests[0].model_dump()
+                    })
 
-        if msg.domain is not None:
-            # Check to see if request is already live
-            all_requests = databases.AirlineVerification.find(
-                        (databases.AirlineVerification.airline_callsign == msg.airline_callsign) &
-                        (databases.AirlineVerification.network == msg.network)
-                    ).all()
-            if len(all_requests) > 0:
-                common.logger.info(f"Request already exists for {all_requests[0].model_dump()}")
-                return JSONResponse(
-                    status_code=403,
-                    content={
-                        "error": "Request already exists",
-                        "data": all_requests[0].model_dump()
-                        })
+        # Generate a random verification token
+        verification_token = secrets.token_urlsafe(32)
 
-            # Generate a random verification token
-            verification_token = secrets.token_urlsafe(32)
-
-            # Store the verification token in Redis with a TTL of 24 hours
-            verification = databases.AirlineVerification(
-                verification_token=verification_token,
-                network=msg.network,
-                airline_name=msg.airline_name,
-                airline_callsign=msg.airline_callsign,
-                domain=msg.domain,
+        # Store the verification token in Redis with a TTL of 24 hours
+        verification = databases.AirlineVerification(
+            verification_token=verification_token,
+            network=msg.network,
+            airline_name=msg.airline_name,
+            airline_callsign=msg.airline_callsign,
+            domain=msg.domain,
+        )
+        verification.save()
+        databases.redis_db.expire(
+            verification.key(),
+            86400,
+        )
+        return templates.TemplateResponse(
+            request=request,
+            name="airline_domain_verification.html",
+            context={"verification_token": verification_token, "domain": msg.domain}
             )
-            verification.save()
-            databases.redis_db.expire(
-                verification.key(),
-                86400,
-            )
-            return templates.TemplateResponse(
-                request=request,
-                name="airline_domain_verification.html",
-                context={"verification_token": verification_token, "domain": msg.domain}
-                )
-        else:
-            # If no domain provided, just generate temporary API key and return it
-            pass # pragma: no cover
+    else:
+        # If no domain provided, just generate temporary API key and return it
+        pass # pragma: no cover
 
 @router.get("/domain_auth/{verification_token}")
 async def domain_auth_check(verification_token:str, session: databases.SessionDep):
