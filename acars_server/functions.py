24ac36482c7ed8784932cea34f4441d204ec27e9
chssn <33300267+chssn@users.noreply.github.com>
2026-06-05 23:14:36 +0100

    perf: removed unused class

--- acars_server/functions.py
+++ acars_server/functions.py
@@ -7,8 +7,6 @@ Chris Parkinson (@chssn)
 #!/usr/bin/env python3
 
 # Standard Libraries
-import time
-from collections import deque
 from typing import Any, Dict
 
 # Third Party Libraries
@@ -16,30 +14,6 @@ import requests # type: ignore
 from loguru import logger
 
 # Local Libraries
-
-
-class RateLimiter:
-    """A rate limiter"""
-    def __init__(self, window_minutes: int, limit: int):
-        self.window = window_minutes * 60  # convert to seconds
-        self.limit = limit
-        self.requests:deque = deque()
-
-    def is_rate_limited(self) -> bool:
-        """Check if a rate limit has been exceeded"""
-        now = time.time()
-
-        # Remove old requests outside the window
-        while self.requests and self.requests[0] < now - self.window:
-            self.requests.popleft()
-
-        # Check limit
-        if len(self.requests) >= self.limit:
-            return True
-
-        # Record this request
-        self.requests.append(now)
-        return False
 
 
 def load_json_url(url: str, timeout: int = 5, headers: dict|None = None) -> Dict[str, Any]:
