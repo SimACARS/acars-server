0cd6192203dc30d3b42b89e3e49345354588fd91
chssn <chris529@gmail.com>
2026-06-20 17:23:38 +0000

    chore: added alembic for db migrations

--- acars_server/__init__.py
+++ acars_server/__init__.py
@@ -6,3 +6,15 @@ Chris Parkinson (@chssn)
 #!/usr/bin/env python3
 
 __VERSION__ = "2.2.0-alpha.3"
+__ALL__ = [
+    "api",
+    "auth",
+    "common",
+    "config",
+    "databases",
+    "functions",
+    "networks",
+    "rcp",
+    "static_data",
+    "tasks"
+]
