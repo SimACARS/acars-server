b632b665e5ee527f86e6837399133e7500d75b6f
chssn <chris529@gmail.com>
2026-06-21 20:45:05 +0000

    fix: this should also be a 202 response

--- acars_server/api/routes/acars.py
+++ acars_server/api/routes/acars.py
@@ -115,7 +115,7 @@ async def hoppie_formated_url(
 
 @router.post(
         "/tx/{bearer}",
-        status_code=201,
+        status_code=202,
         responses=static_data.COMMON_ERRORS,
         response_model=databases.StoreAndForward
         )
